74308378c5c51ab18cf024594768cc33ef8a61b4
Naoto Shimazaki <nshimaza@cisco.com>
2016-02-23 09:59:10 +0900

    fixed some leakage

---
+++ app/controllers/AgentStateController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject._
+
+import models.{ AgentStateMap, AgentStateMapFactory }
+import play.api.Play
+import play.api.libs.json.Json
+import play.api.mvc._
+
+/**
+  * Created by nshimaza on 2016/02/21.
+  */
+@Singleton
+class AgentStateController @Inject()(agentStateMapFactory: AgentStateMapFactory) extends Controller {
+  val extLow = Play.current.configuration.getInt("uccertweb.ext.low").get
+  val extHigh = Play.current.configuration.getInt("uccertweb.ext.high").get
+
+  val agentMap = agentStateMapFactory(extLow to extHigh)
+
+  def extension(ext: Int) = Action {
+    try {
+      val (state, reason) = agentMap.get(ext).get
+      val json = Json.obj("state" -> state.toString, "reason" -> reason)
+      Ok(json.toString)
+    } catch {
+      case e:  NoSuchElementException => NotFound("404 No such extension")
+    }
+  }
+}
---
+++ test/controllers/AgentStateControllerSpec.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import org.scalatestplus.play._
+
+
+/**
+  * Created by nshimaza on 2016/02/21.
+  */
+class AgentStateControllerSpec extends PlaySpec {
+  "AgentState" must {
+    "return 404 on unconfigured extension" in {
+    }
+
+    "return LOGOUT as initial state" in {
+    }
+
+    "return current agent state" in {
+    }
+  }
+
+}
